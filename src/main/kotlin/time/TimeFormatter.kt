752606377ecf2067eaeaa0730ad5b0ae84733fc9
Gareth Rowlands <gareth.rowlands@johnlewis.co.uk>
2019-03-23 18:20:04 +0000

    The class is not doing anything so get rid of it.

--- src/main/kotlin/time/TimeFormatter.kt
+++ src/main/kotlin/time/TimeFormatter.kt
@@ -1,63 +1,56 @@
 package time
 
-class TimeFormatter {
+private const val second = 1
+private const val minuteInSeconds = 60 * second
+private const val hourInSeconds = minuteInSeconds * 60
+private const val dayInSeconds = hourInSeconds * 24
+private const val yearInSeconds = dayInSeconds * 365
 
-    companion object {
-        const val second = 1
-        const val minuteInSeconds = 60 * second
-        const val hourInSeconds = minuteInSeconds * 60
-        const val dayInSeconds = hourInSeconds * 24
-        const val yearInSeconds = dayInSeconds * 365
+private val periods = listOf(
+        Pair("year", yearInSeconds),
+        Pair("day", dayInSeconds),
+        Pair("hour", hourInSeconds),
+        Pair("minute", minuteInSeconds),
+        Pair("second", second)
+)
 
-        val periods = listOf(
-                Pair("year", yearInSeconds),
-                Pair("day", dayInSeconds),
-                Pair("hour", hourInSeconds),
-                Pair("minute", minuteInSeconds),
-                Pair("second", second)
-        )
-    }
+fun timeFormat(input: Int): String {
+    if (input == 0) return "none"
 
+    val resultsToFormat = findTimeUnitsAndMultiples(input)
 
-    fun timeFormat(input: Int): String {
-        if (input == 0) return "none"
-
-        val resultsToFormat = findTimeUnitsAndMultiples(input)
-
-        val unitsAndCountsFormatted = resultsToFormat
-                .map { (unit, unitCount) ->
-                    formatUnits(unit, unitCount)
-                }
-        val timeInWords = toStringWithCommasAndAnd(unitsAndCountsFormatted)
-
-        return timeInWords
-    }
-
-    private fun findTimeUnitsAndMultiples(input: Int): List<Pair<String, Int>> {
-        var remainingSeconds = input
-        val calculatedPeriods = mutableListOf<Pair<String, Int>>()
-        periods.forEach { (periodName, periodSeconds) ->
-            calculatedPeriods.add(periodName to (remainingSeconds / periodSeconds))
-            remainingSeconds %= periodSeconds
-        }
-        return calculatedPeriods.filter { it.second != 0 }
-    }
-
-    fun formatUnits(unitkey: String, unitvalue: Int): String = "$unitvalue $unitkey" + plural(unitvalue)
-
-    private fun toStringWithCommasAndAnd(unitsAndCountsFormatted: List<String>): String = unitsAndCountsFormatted
-            .zip((unitsAndCountsFormatted.size - 1) downTo 0)
-            .map { (formattedUnit, countUnitsToDisplay) ->
-                formattedUnit + formatAndOrComma(countUnitsToDisplay)
+    val unitsAndCountsFormatted = resultsToFormat
+            .map { (unit, unitCount) ->
+                formatUnits(unit, unitCount)
             }
-            .joinToString("")
+    val timeInWords = toStringWithCommasAndAnd(unitsAndCountsFormatted)
 
-    private fun plural(unit: Int): String = if (unit == 1) "" else "s"
+    return timeInWords
+}
 
-    private fun formatAndOrComma(count: Int): String = when (count) {
-        0 -> ""
-        1 -> " and "
-        else -> ", "
+private fun findTimeUnitsAndMultiples(input: Int): List<Pair<String, Int>> {
+    var remainingSeconds = input
+    val calculatedPeriods = mutableListOf<Pair<String, Int>>()
+    periods.forEach { (periodName, periodSeconds) ->
+        calculatedPeriods.add(periodName to (remainingSeconds / periodSeconds))
+        remainingSeconds %= periodSeconds
     }
+    return calculatedPeriods.filter { it.second != 0 }
+}
 
+private fun formatUnits(unitkey: String, unitvalue: Int): String = "$unitvalue $unitkey" + plural(unitvalue)
+
+private fun toStringWithCommasAndAnd(unitsAndCountsFormatted: List<String>): String = unitsAndCountsFormatted
+        .zip((unitsAndCountsFormatted.size - 1) downTo 0)
+        .map { (formattedUnit, countUnitsToDisplay) ->
+            formattedUnit + formatAndOrComma(countUnitsToDisplay)
+        }
+        .joinToString("")
+
+private fun plural(unit: Int): String = if (unit == 1) "" else "s"
+
+private fun formatAndOrComma(count: Int): String = when (count) {
+    0 -> ""
+    1 -> " and "
+    else -> ", "
 }
--- src/test/kotlin/time/TimeFormatterTest.kt
+++ src/test/kotlin/time/TimeFormatterTest.kt
@@ -5,21 +5,19 @@ import kotlin.test.assertEquals
 
 class TimeFormatterTest {
 
-    val underTest = TimeFormatter()
-
     @Test
     fun `should return none for an input zero`() {
-            assertEquals("none", underTest.timeFormat(0))
+            assertEquals("none", timeFormat(0))
     }
 
 
     @Test
     fun `should return formatted time for a valid input`() {
-        assertEquals("1 second", underTest.timeFormat(1))
-        assertEquals("1 minute and 2 seconds", underTest.timeFormat(62))
-        assertEquals("1 hour, 1 minute and 2 seconds", underTest.timeFormat(3662))
-        assertEquals("3 years and 24 minutes", underTest.timeFormat(94609440))
-        assertEquals("3 days, 4 hours and 1 minute", underTest.timeFormat(273660))
+        assertEquals("1 second", timeFormat(1))
+        assertEquals("1 minute and 2 seconds", timeFormat(62))
+        assertEquals("1 hour, 1 minute and 2 seconds", timeFormat(3662))
+        assertEquals("3 years and 24 minutes", timeFormat(94609440))
+        assertEquals("3 days, 4 hours and 1 minute", timeFormat(273660))
     }
 
 }
